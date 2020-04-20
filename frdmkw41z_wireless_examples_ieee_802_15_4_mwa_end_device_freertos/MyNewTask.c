fa0d1117f8274b405ace6a89cb63b69b6e9612f1
IE707729 <ie707729@iteso.mx>
2020-04-19 23:40:22 -0500

    Add files via upload

---
+++ frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/MyNewTask.c
@@ -0,0 +1,104 @@
+/*
+ * MyNewTask.c
+ *
+ *  Created on: 19 abr. 2020
+ *      Author: edson
+ */
+#include "MyNewTask.h"
+
+osaEventId_t mMyEvents;
+/* Global Variable to store our TimerID */
+tmrTimerID_t myTimerID = gTmrInvalidTimerID_c;
+/* Handler ID for task */
+osaTaskId_t gMyTaskHandler_ID;
+/* Local variable to store the current state of the LEDs */
+static uint8_t ledsState = 0;
+
+static void myTaskTimerCallback(void *param)
+{
+	OSA_EventSet(mMyEvents, gMyNewTaskEvent2_c);
+}
+/* Main custom task */
+void Task_Counter(osaTaskParam_t argument)
+{
+	osaEventFlags_t customEvent;
+	myTimerID = TMR_AllocateTimer();
+	uint8_t counter = 0;
+	while(1)
+	{
+		OSA_EventWait(mMyEvents, osaEventFlagsAll_c, FALSE, osaWaitForever_c,&customEvent);
+		if( !gUseRtos_c && !customEvent)
+		{
+			break;
+		}
+		/* Depending on the received event */
+		switch(customEvent){
+		case gMyNewTaskEvent1_c:
+			TMR_StartIntervalTimer(myTimerID, /*myTimerID*/
+					3000, /* Timer's Timeout  */
+					myTaskTimerCallback, /* pointer to myTaskTimerCallback function */
+					NULL
+			);
+			TurnOffLeds(); /* Ensure all LEDs are turned off */
+			break;
+		case gMyNewTaskEvent2_c: /* Event called from myTaskTimerCallback */
+
+			if(counter == 0)
+			{
+				TurnOffLeds();
+				Led_RGB_Red(LED1,1);
+				counter++;
+				break;
+			}
+
+			if(counter == 1)
+			{
+				TurnOffLeds();
+				Led_RGB_Green(LED1,1);
+				counter++;
+				break;
+			}
+
+			if(counter == 2)
+			{
+				TurnOffLeds();
+				Led_RGB_Blue(LED1,1);
+				counter++;
+				break;
+
+			}
+
+			if(counter == 3)
+			{
+				turnOffLeds();
+				TurnOnLeds();
+				counter = 0;
+				break;
+			}
+		case gMyNewTaskEvent3_c: /* Event to stop the timer */
+			ledsState = 0;
+			TurnOffLeds();
+			TMR_StopTimer(myTimerID);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+OSA_TASK_DEFINE(Task_Counter, gMyTaskPriority_c, 1, gMyTaskStackSize_c, FALSE );
+
+
+
+
+void MyTask_Init(void)
+{
+mMyEvents = OSA_EventCreate(TRUE);
+/* The instance of the MAC is passed at task creaton */
+gMyTaskHandler_ID = OSA_TaskCreate(OSA_TASK(Task_Counter), NULL);
+}
+
+void MyTaskTimer_Stop(void)
+{
+OSA_EventSet(mMyEvents, gMyNewTaskEvent3_c);
+}
